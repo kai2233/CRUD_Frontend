d68936d02da47592f10ee8258d455d4cb752cd6b
Kaifeng <1536771545kai@gmail.com>
2023-06-30 14:49:58 -0400

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+// Pages
+import Homepage from "./pages/Homepage";
+import Campuses from "./pages/Campuses";
+import Students from "./pages/Students";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Link to="/">Home</Link><br/>
+        <Link to="/allCampuses">Campuses</Link><br/>
+        <Link to="/allStudents">Students</Link><br/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/allCampuses" element={<Campuses />} />
+        <Route path="/allStudents" element={<Students />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Students.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// Just my home page
+
+const Students = () => {
+  return (
+    <div>
+      <h1>Hi! this is students page</h1>
+    </div>
+  );
+};
+
+export default Students;

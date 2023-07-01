c0ef6aecf72fe27d20d0bb322c1fe7c7ddb285aa
Kaifeng <1536771545kai@gmail.com>
2023-06-30 23:37:09 -0400

    upload display all campus feature

---
+++ src/components/DisplayCampuses.jsx
@@ -0,0 +1,15 @@
+const displayList = (props) =>{
+    return(
+        <div>
+            {props.list.map(data=>{
+                return(
+                    <div key = {data.id}>
+                        <img src = {data.imageUrl}/>
+                        <h1>{data.name}</h1>
+                    </div>
+                )
+            })}  
+        </div>
+    )
+}
+export default displayList;
--- src/pages/Campuses.jsx
+++ src/pages/Campuses.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-// Just my home page
+import React, {useEffect} from "react";
+import {useSelector,useDispatch} from "react-redux";
+import {fetchAllCampusesThunk} from "../redux/campuses/campuses.action";
+import DisplayCampuses from '../components/DisplayCampuses';
 
 const Campuses = () => {
+  const allCampus = useSelector((state) => state.campuses.allCampuses);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllCampusesThunk());
+  }, []);
+  
   return (
     <div>
       <h1>Hi! this is campuses page</h1>
+      <DisplayCampuses list= {allCampus}/>
     </div>
   );
 };

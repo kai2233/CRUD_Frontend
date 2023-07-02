6fcdf2e64a0fcebe0e5c607810f9da5774188bb8
Kaifeng <1536771545kai@gmail.com>
2023-07-02 19:35:12 -0400

    update add campus page

--- src/pages/AddCampus.jsx
+++ src/pages/AddCampus.jsx
@@ -1,41 +1,42 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addNewCampusThunk } from "../redux/campuses/campuses.action";
-import { Link } from "react-router-dom";
+
 
 const AddCampus = () => {
-    const newCampus = useSelector((state) => state.campuses.newCampus);
     const dispatch = useDispatch();
-    const [name, setName] = useState("");
+    const [name, setName] = useState('');
     const [imageUrl, setImageUrl] = useState();
-    const [address, setAddress] = useState("");
-    const [description, setDescription] = useState("");
+    const [address, setAddress] = useState('');
+    const [description, setDescription] = useState('');
     const addNewCampus = (e) => {
         e.preventDefault();
-        const campus = {
+        const createCampus = {
             name,
             imageUrl,
             address,
             description,
         };
-        console.log(campus);
-        name!==''&&address!==''?(dispatch(addNewCampusThunk(campus))):( <h1>NAME OR ADDRESS IS EMPTY</h1>);
+        console.log(createCampus);
+        dispatch(addNewCampusThunk(createCampus));
     };
+    const newCampus = useSelector((state) => state.campuses.newCampus);
     return (
         <div>
             <h1>Hi! this is Add Campus page</h1>
-            <form>
-                <label>Name</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                <label>Image Url</label>
-                <input type="text" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
-                <label>Address</label>
-                <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
-                <label>Description</label>
-                <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-                <button type="submit" onClick={addNewCampus}>Submit</button>
-                {console.log(newCampus)}
+            <form onSubmit={addNewCampus}>
+                <label>Name </label>
+                <input type="text" required value={name} placeholder="name..." onChange={(e) => setName(e.target.value)}/><br/>
+                <label>Address </label>
+                <input type="text" required value={address} placeholder="address..."onChange={(e) => setAddress(e.target.value)}/><br/>
+                <label>Image Url </label>
+                <input type="text" value={imageUrl} placeholder="url..." onChange={(e) => setImageUrl(e.target.value)}/><br/>
+                <label>Description </label>
+                <input type="text" value={description} placeholder="description..." onChange={(e) => setDescription(e.target.value)}/><br/>
+                {/* <button type="submit" onClick={addNewCampus}>Submit</button> */}
+                <input type="submit"/>
             </form>
+            {newCampus.name===name?(<h1>created successfully</h1>):(null)}
         </div>
     )
 }

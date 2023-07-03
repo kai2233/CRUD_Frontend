6b8b776104aff5f409167ebb06882d53a0f43b6d
Kaifeng <1536771545kai@gmail.com>
2023-07-03 13:48:02 -0400

    redirect to add student form from all student view

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllStudentsThunk} from "../redux/students/students.action";
 import DisplayStudents from "../components/DisplayStudents";
+import { Link } from "react-router-dom";
 const Students = () => {
 
   const allStudents = useSelector((state) => state.students.allStudents);
@@ -10,10 +11,11 @@ const Students = () => {
   useEffect(() => {
     dispatch(fetchAllStudentsThunk());
   }, []);
-  
+
   return (
     <div>
       <h1>Hi! this is students page</h1>
+      <Link to = "/addStudent"><button>click to add students</button></Link>
       {allStudents.length!==0?(<DisplayStudents/>):(<h3>No students found</h3>)}
     </div>
   );

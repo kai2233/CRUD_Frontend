4c49cc1cfc4608c73cac0109bf17762964794b5e
Kaifeng <1536771545kai@gmail.com>
2023-07-03 14:04:26 -0400

    redirect to single student view

--- src/components/DisplaySingleCampus.jsx
+++ src/components/DisplaySingleCampus.jsx
@@ -1,38 +1,23 @@
-import {useSelector} from "react-redux";
-const DisplaySingleCampus = (props) => {
-    // console.log(props.Campus);
-    // console.log(props.Campus[0].name);
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+const DisplaySingleCampus = () => {
     const Campus = useSelector(state => state.campuses.singleCampus);
     return (
         <div >
-            {/* <img src={props.Campus[0].imageUrl} />
-            <h1>{props.Campus[0].name}</h1>
-            <h2>{props.Campus[0].address}</h2>
-            <h3>{props.Campus[0].description}</h3>
-            {props.Campus[0].students.map(data=>{
-                return(
-                    <div key = {data.id}>
-                        <img src = {data.imageUrl}/>
-                        <h1>{data.firstName}</h1>
-                        <h1>{data.lastName}</h1>
-                    </div>
-                )
-            })}   */}
             <img src={Campus[0].imageUrl} />
             <h1>{Campus[0].name}</h1>
             <h2>{Campus[0].address}</h2>
             <h3>{Campus[0].description}</h3>
-            {Campus[0].students.map(data=>{
-                return(
-                    <div key = {data.id}>
-                        <img src = {data.imageUrl}/>
-                        <h1>{data.firstName}</h1>
-                        <h1>{data.lastName}</h1>
+            {Campus[0].students.map(data => {
+                return (
+                    <div key={data.id}>
+                        <img src={data.imageUrl} />
+                        <Link to="/singleStudent" state={{ studentId: data.id }}>
+                            {data.firstName} {data.lastName}
+                        </Link>
                     </div>
                 )
-            })}  
-            
-
+            })}
         </div>
     )
 }

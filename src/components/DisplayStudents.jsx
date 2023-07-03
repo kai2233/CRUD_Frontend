bd5532eae6bb05919b5c018d0b2c20b1bd9040d5
Kaifeng <1536771545kai@gmail.com>
2023-07-03 13:53:49 -0400

    add delete student feature

--- src/components/DisplayStudents.jsx
+++ src/components/DisplayStudents.jsx
@@ -1,8 +1,14 @@
 import {useSelector,useDispatch} from "react-redux";
 import {Link} from "react-router-dom";
+import { deleteStudentThunk } from "../redux/students/students.action";
 const DisplayStudents = () =>{
     const allStudents = useSelector((state) => state.students.allStudents);
+    const dispatch = useDispatch();
 
+    const deleteStudent = (id) => {
+        dispatch(deleteStudentThunk(id));
+        console.log("deleted student id: "+id)
+    }
     return(
         <div>
             {allStudents.map(data=>{
@@ -10,6 +16,7 @@ const DisplayStudents = () =>{
                     <div key = {data.id}>
                         <img src = {data.imageUrl}/>
                         <Link to = "/singleStudent" state ={{studentId:data.id}}>{data.firstName} {data.lastName}</Link>
+                        <button onClick = {()=>deleteStudent(data.id)}>X</button>
                     </div>
                 )
             })}  

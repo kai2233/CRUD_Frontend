3606e39ce4be308f57d4054562e3fc5e37a8482c
Kaifeng <1536771545kai@gmail.com>
2023-07-03 14:20:14 -0400

    modify message

--- src/components/DisplaySingleStudent.jsx
+++ src/components/DisplaySingleStudent.jsx
@@ -8,7 +8,7 @@ const DisplaySingleStudent = () => {
             <h2>Email: {student[0].email}</h2>
             <h3>gpa: {student[0].gpa}</h3>
             <h3>Enrolled Campus:</h3>
-            {student[0].campusId!==null?(<h2>{student[0].campus.name}</h2>):(<h4>THIS STUDENT DIDN'T ENROLL ANY CAMPUS</h4>)}
+            {student[0].campusId!==null?(<h2>{student[0].campus.name}</h2>):(<h4>THIS STUDENT DIDN'T ENROLL IN ANY CAMPUS</h4>)}
         </div>
     );
 

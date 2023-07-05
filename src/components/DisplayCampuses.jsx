565441e10bf66999c021436e6c150eea9be1eb17
Kaifeng <1536771545kai@gmail.com>
2023-07-05 17:04:47 -0400

    add styling to all campuses view

--- src/components/DisplayCampuses.jsx
+++ src/components/DisplayCampuses.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import {useSelector,useDispatch} from "react-redux";
 import {deleteCampusThunk} from "../redux/campuses/campuses.action"
+import "../pages/campusesPages/campusesIndex.css"
 const DisplayList = () =>{
     const allCampus = useSelector((state) => state.campuses.allCampuses);
     const dispatch = useDispatch();
@@ -13,14 +14,20 @@ const DisplayList = () =>{
         <div>
             {allCampus.map(data=>{
                 return(
-                    <div key = {data.id}>
+                    <div className="subContainer" key = {data.id}>
+                        <div>
                         <img src = {data.imageUrl}/>
+                        </div>
+                        <div className="divblock">
                         <Link to="/singleCampus" state={{ campID:data.id }}>
                             {data.name}
                         </Link>
+                        </div>
                         {/* wrap up function wanted to execute by button onClick otherwise it will automatically executed*/}
                         {/*onClick ={deleteCampus} is just passing in the invocation of function instead of definition of function*/}
+                        <div className="divblock">
                         <button onClick = {() =>deleteCampus(data.id)}>X</button>
+                        </div>
                     </div>
                 )
             })}  
--- src/pages/campusesPages/Campuses.jsx
+++ src/pages/campusesPages/Campuses.jsx
@@ -3,7 +3,7 @@ import {useSelector,useDispatch} from "react-redux";
 import {fetchAllCampusesThunk} from "../../redux/campuses/campuses.action";
 import { Link } from "react-router-dom";
 import DisplayCampuses from '../../components/DisplayCampuses';
-
+import "./campusesIndex.css"
 const Campuses = () => {
   const allCampus = useSelector((state) => state.campuses.allCampuses);
   const dispatch = useDispatch();
@@ -12,10 +12,10 @@ const Campuses = () => {
     dispatch(fetchAllCampusesThunk());
   }, []);
   return (
-    <div>
+    <div className="root-container"> 
       <h1>All Campuses</h1>
       {/* <Link to ="/singleCampus">Single Campus</Link><br/> */}
-      <Link to = "/addCampus"><button type="button">click to add campus</button></Link>
+      <Link className="a-inside-root-container" to = "/addCampus"><button>click to add campus</button></Link>
       {allCampus.length!==0?(<DisplayCampuses/>):(<h3>There are no campuses registered in the database</h3>)}
     </div>
   );

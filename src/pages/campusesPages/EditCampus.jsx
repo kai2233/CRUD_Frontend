0238dd457f4fc5dce8168faaca43fe08fdc24c51
Kaifeng <1536771545kai@gmail.com>
2023-07-05 19:44:25 -0400

    add css to the edit campus form

--- src/pages/campusesPages/EditCampus.jsx
+++ src/pages/campusesPages/EditCampus.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateCampusThunk } from "../../redux/campuses/campuses.action";
 import { useLocation, Link } from "react-router-dom";
-
+import "./index.css";
 
 const EditCampus = () => {
     const location = useLocation();
@@ -26,17 +26,19 @@ const EditCampus = () => {
     };
     // const targetCampus = useSelector((state) => state.campuses.updatedCampus);
     return (
-        <div>
+        <div className="edit-campus-page-container">
             <h1>Edit Campus Form</h1>
             <form onSubmit={AlterCampus}>
                 <label>Name </label>
-                <input type="text"   placeholder="name..." onChange={(e) => setName(e.target.value)}/><br/>
+                <input type="text"   placeholder="name..." onChange={(e) => setName(e.target.value)}/>
                 <label>Address </label>
-                <input type="text"   placeholder="address..."onChange={(e) => setAddress(e.target.value)}/><br/>
+                <input type="text"   placeholder="address..."onChange={(e) => setAddress(e.target.value)}/>
                 <label>Image Url </label>
-                <input type="text"  placeholder="url..." onChange={(e) => setImageUrl(e.target.value)}/><br/>
+                <input type="text"  placeholder="url..." onChange={(e) => setImageUrl(e.target.value)}/>
                 <label>Description </label>
-                <input type="text"  placeholder="description..." onChange={(e) => setDescription(e.target.value)}/><br/>
+                <div>
+                    <textarea className="form-description" type="text"  placeholder="description..." onChange={(e) => setDescription(e.target.value)}/>
+                </div>
                 <input type="submit"/>
             </form>
             {/* {targetCampus.id===campID?(<h1>Edited Successfully</h1>):(null)} */}

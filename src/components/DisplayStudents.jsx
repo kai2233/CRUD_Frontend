8ab5cc5a89a3d82a91471511c96f32bdbbe902d6
Kaifeng <1536771545kai@gmail.com>
2023-07-05 20:15:33 -0400

    add css to all students view

--- src/components/DisplayStudents.jsx
+++ src/components/DisplayStudents.jsx
@@ -1,25 +1,29 @@
-import {useSelector,useDispatch} from "react-redux";
-import {Link} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { deleteStudentThunk } from "../redux/students/students.action";
-const DisplayStudents = () =>{
+import "../pages/studentsPages/index.css";
+
+const DisplayStudents = () => {
     const allStudents = useSelector((state) => state.students.allStudents);
     const dispatch = useDispatch();
 
     const deleteStudent = (id) => {
         dispatch(deleteStudentThunk(id));
-        console.log("deleted student id: "+id)
+        console.log("deleted student id: " + id)
     }
-    return(
+    return (
         <div>
-            {allStudents.map(data=>{
-                return(
-                    <div key = {data.id}>
-                        <img src = {data.imageUrl}/>
-                        <Link to = "/singleStudent" state ={{studentId:data.id}}>{data.firstName} {data.lastName}</Link>
-                        <button onClick = {()=>deleteStudent(data.id)}>X</button>
+            {allStudents.map(data => {
+                return (
+                    <div className="display-all-students" key={data.id}>
+                        <img src={data.imageUrl} />
+                        <div>
+                            <Link to="/singleStudent" state={{ studentId: data.id }}>{data.firstName} {data.lastName}</Link>
+                            <button onClick={() => deleteStudent(data.id)}>X</button>
+                        </div>
                     </div>
                 )
-            })}  
+            })}
         </div>
     )
 }
--- src/pages/studentsPages/Students.jsx
+++ src/pages/studentsPages/Students.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchAllStudentsThunk} from "../../redux/students/students.action";
 import DisplayStudents from "../../components/DisplayStudents";
 import { Link } from "react-router-dom";
+import "./index.css";
 const Students = () => {
 
   const allStudents = useSelector((state) => state.students.allStudents);
@@ -13,7 +14,7 @@ const Students = () => {
   }, []);
 
   return (
-    <div>
+    <div className="all-student-view-container">
       <h1>All Students</h1>
       <Link to = "/addStudent"><button>click to add students</button></Link>
       {allStudents.length!==0?(<DisplayStudents/>):(<h3>No students found</h3>)}
